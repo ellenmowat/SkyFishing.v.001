bf7468523425326745ed9f77578bf50e3e597812
kayleew702 <60621712+kayleew702@users.noreply.github.com>
2020-04-28 12:16:55 -0700

    more water

---
+++ Assets/Scripts/FrogController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        BoundMovement();
+        
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        float moveX = x * speed;
+        float moveY = y * speed;
+
+        rb.velocity = new Vector2(moveX, moveY);
+    }
+
+    void BoundMovement()
+    {
+        float dist = (this.transform.position - Camera.main.transform.position).z;
+
+        float leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
+        float rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
+
+        Vector3 frogSize = GetComponent<Renderer>().bounds.size;
+
+        this.transform.position = new Vector3(Mathf.Clamp
+            (this.transform.position.x, leftBorder + frogSize.x / 2, rightBorder - 1 - frogSize.x / 2),
+            this.transform.position.y, this.transform.position.z);
+    }
+}
